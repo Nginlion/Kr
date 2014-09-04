036f01a8b602bbc915681cfa33e6400ea1d4475c
Nginlion <nginlion@gmail.com>
2014-09-04 13:25:59 +0800

    BigPipe Demo + Combine JS

---
+++ htdocs/default/asserts/js/xe/core/dom/select.js
@@ -0,0 +1,9 @@
+/**
+ * Created by ng on 14-9-4.
+ */
+
+$Import('core.lib.sizzle');
+
+xe.register('core.dom.selector', function($) {
+
+})
---
+++ htdocs/default/asserts/js/xe/xe.js
@@ -0,0 +1,8 @@
+/**
+ * Created by ng on 14-9-4.
+ */
+
+$Import('core.lib.sizzle');
+$Import('core.dom.select');
+
+var Xe = {};
